90f5083f6e039179b94699393f04621ec711109d
Jim Liu <liuz@uchicago.edu>
2022-03-17 00:32:14 -0500

    made links clickable

--- js/fog.js
+++ js/fog.js
@@ -2,7 +2,14 @@ var s = skrollr.init();
 if (s.isMobile() || (window.Width < 913)) {
     s.destroy();
 }
-
+function vw(v) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (v * w) / 100;
+  }
+function vh(v) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (v * h) / 100;
+  }
 window.addEventListener("scroll", function(){
     console.log(window.innerWidth)
     if (s.isMobile() || (window.innerWidth < 913)) {
@@ -18,4 +25,10 @@ window.addEventListener("scroll", function(){
             var pos=window.pageYOffset * target[i].dataset.rate;
             target[i].style.transform='translate('+pos+'px, 0px)';
         }
+    if (window.pageYOffset > vw(130)) {
+        document.querySelector('.chicken').style.height=0;
+    }
+    else {
+        document.querySelector('.chicken').style.height=vh(50);
+    }
 });
